c6dcab6fee8d1e388a85902cb1d2775bca45f24a
veditharv <23f3004007@ds.study.iitm.ac.in>
2025-03-27 00:15:41 +0530

    transaction form

--- backend/src/TransactionValidationServlet.java
+++ backend/src/TransactionValidationServlet.java
@@ -3,6 +3,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
